d3528ebeb8c3f32e7bad6cb64713ff7405ccce55
Odintsov Timur <timodin@yandex.ru>
2020-05-20 15:54:14 +0300

    posts.views: add group_posts() func

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,18 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-from .models import Post
+from .models import Post, Group
 
 
 def index(request):
     latest = Post.objects.order_by('-pub_date')[:11]
     output = []
+
     return render(request, "index.html", {"posts": latest})
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = group.posts.order_by("-pub_date")[:12]
+
+    return render(request, "group.html", {"group": group, "posts": posts})

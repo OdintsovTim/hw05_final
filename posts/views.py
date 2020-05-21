5578fb661802037a8bd6f583cbeff41850d76e1d
Odintsov Timur <timodin@yandex.ru>
2020-05-21 20:28:25 +0300

    posts.views: delete order_by

--- posts/views.py
+++ posts/views.py
@@ -5,14 +5,13 @@ from .models import Post, Group
 
 
 def index(request):
-    latest = Post.objects.order_by('-pub_date')[:11]
-    output = []
+    latest = Post.objects.all()[:11]
 
     return render(request, "index.html", {"posts": latest})
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.order_by("-pub_date")[:12]
+    posts = group.posts.all()[:12]
 
     return render(request, "group.html", {"group": group, "posts": posts})

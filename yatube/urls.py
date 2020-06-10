97cc6bf29a2b3d6f8cf0e50ba6c9f8c7e422531f
Odintsov Timur <timodin@yandex.ru>
2020-06-10 14:17:57 +0300

    yatube.urls: add new pathes

--- yatube/urls.py
+++ yatube/urls.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.flatpages import views
 
 
 urlpatterns = [
     path('', include('posts.urls')),
+    path('about/', include('django.contrib.flatpages.urls')),
     path("auth/", include("users.urls")),
     path("auth/", include("django.contrib.auth.urls")),
     path('admin/', admin.site.urls),
+]
+
+urlpatterns += [
+        path('about-us/', views.flatpage, {'url': '/about-us/'}, name='about'),
+        path('terms/', views.flatpage, {'url': '/terms/'}, name='terms'),
+        path('about-author/', views.flatpage, {'url': '/about-author/'}, name='author'),
+        path('about-spec/', views.flatpage, {'url': '/about-spec/'}, name='spec'),
 ]

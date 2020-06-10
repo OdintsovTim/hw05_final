71245030751ae81f84b0c2873297abac8491643a
Odintsov Timur <timodin@yandex.ru>
2020-06-10 14:17:39 +0300

    posts.urls: add new pathes

--- posts/urls.py
+++ posts/urls.py
@@ -5,6 +5,13 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<str:username>/', views.profile, name='profile'),
+    path('<str:username>/<int:post_id>/', views.post_view, name='post'),
+    path(
+        '<str:username>/<int:post_id>/edit/', 
+        views.post_edit, 
+        name='post_edit'
+    ),
     path('group/<slug:slug>', views.group_posts, name='groups'),
     path('new/', views.new_post, name='new_post')
 ]

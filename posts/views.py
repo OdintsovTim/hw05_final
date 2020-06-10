0d2368416cc5edc518674569a2a56b6c1009e220
Odintsov Timur <timodin@yandex.ru>
2020-06-10 14:21:43 +0300

    changes from theory

--- posts/views.py
+++ posts/views.py
@@ -1,21 +1,29 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 from .forms import PostForm
 from .models import Post, Group
 
 
 def index(request):
-    latest = Post.objects.all()[:11]
+    latest = Post.objects.all()
+    paginator = Paginator(latest, 10)
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)
 
-    return render(request, "index.html", {"posts": latest})
+
+    return render(request, "index.html", {'page': page, 'paginator': paginator})
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.all()[:12]
+    posts = group.posts.all()
+    paginator = Paginator(posts, 4)
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)
 
-    return render(request, "group.html", {"group": group, "posts": posts})
+    return render(request, "group.html", {"group": group, 'page': page, 'paginator': paginator})
 
 
 @login_required
@@ -33,3 +41,21 @@ def new_post(request):
             return redirect('index')
 
     return render(request, "new_post.html", {'form': form})
+
+
+def profile(request, username):
+        # тут тело функции
+        return render(request, 'profile.html', {})
+ 
+ 
+def post_view(request, username, post_id):
+        # тут тело функции
+        return render(request, 'post.html', {})
+
+
+def post_edit(request, username, post_id):
+        # тут тело функции. Не забудьте проверить, 
+        # что текущий пользователь — это автор записи.
+        # В качестве шаблона страницы редактирования укажите шаблон создания новой записи
+        # который вы создали раньше (вы могли назвать шаблон иначе)
+        return render(request, 'new_post.html', {})

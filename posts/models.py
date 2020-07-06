aff079c659e149daa3061eaf83c01b5d98a28134
Odintsov Timur <timodin@yandex.ru>
2020-07-07 00:38:43 +0300

    posts.models: add image field to Post() class

--- posts/models.py
+++ posts/models.py
@@ -18,6 +18,7 @@ class Post(models.Model):
     pub_date = models.DateTimeField("date published", auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     group = models.ForeignKey(Group, on_delete=models.SET_NULL, blank=True, null=True, related_name='posts')
+    image = models.ImageField(upload_to='posts/', blank=True, null=True)
 
     class Meta:
         ordering = ['-pub_date']

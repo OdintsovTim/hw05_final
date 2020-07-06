5708ac813900e8376a916e3f7bd19282cc4531ce
Odintsov Timur <timodin@yandex.ru>
2020-07-07 00:38:17 +0300

    posts.forms: add CommentForm() class

--- posts/forms.py
+++ posts/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.forms import ModelForm
 
-from .models import Post
+from .models import Post, Comment
 
 
 class PostForm(ModelForm):
@@ -26,3 +26,9 @@ class PostForm(ModelForm):
             raise forms.ValidationError('Слишком короткий текст, мы любим графоманов!')
 
         return data
+
+
+class CommentForm(ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['text']

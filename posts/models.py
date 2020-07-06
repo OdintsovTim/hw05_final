dc8d130d81359128b116b59a6437c625414b29d6
Odintsov Timur <timodin@yandex.ru>
2020-07-07 00:39:06 +0300

    posts.models: add Comment() class

--- posts/models.py
+++ posts/models.py
@@ -22,3 +22,10 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-pub_date']
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField(max_length=200)
+    pub_date = models.DateTimeField("date published", auto_now_add=True)

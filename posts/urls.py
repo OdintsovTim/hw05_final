3d8d06a382a10734a918cd1e1ac11a7e5a6aac9a
Odintsov Timur <timodin@yandex.ru>
2020-07-07 00:40:53 +0300

    posts.urls: add pathes for 404 and 500 pages

--- posts/urls.py
+++ posts/urls.py
@@ -5,7 +5,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('group/<slug:slug>', views.group_posts, name='groups'),
+    path('404/', views.page_not_found, kwargs={'exception': Exception('Cтраница не найдена')}, name='404'),
+    path('500', views.server_error, name='500'),
+    path('group/<slug:slug>', views.group_posts, name='group'),
     path('new/', views.new_post, name='new_post'),
     path('<str:username>/', views.profile, name='profile'),
     path('<str:username>/<int:post_id>/', views.post_view, name='post'),

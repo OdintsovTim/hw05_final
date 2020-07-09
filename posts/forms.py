8b4817c4b93c304bd25d318f404665a7ad151f4a
Odintsov Timur <timodin@yandex.ru>
2020-07-09 14:37:16 +0300

    posts.forms: change type of fields from list to tuple

--- posts/forms.py
+++ posts/forms.py
@@ -7,7 +7,7 @@ from .models import Post, Comment
 class PostForm(ModelForm):
     class Meta:
         model = Post
-        fields = ['group', 'text', 'image']
+        fields = ('group', 'text', 'image')
         labels = {
             'group': 'Группа',
             'text': 'Ваш текст',
@@ -31,4 +31,4 @@ class PostForm(ModelForm):
 class CommentForm(ModelForm):
     class Meta:
         model = Comment
-        fields = ['text']
+        fields = ('text',)
